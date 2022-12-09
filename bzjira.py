787e47d7fe1dfd4a03774bad3ba8facec59a1cb8
Honza Horak <hhorak@redhat.com>
2022-12-10 00:26:26 +0100

    Implement move and clone commands

--- bzjira.py
+++ bzjira.py
@@ -38,16 +38,26 @@ def get_auth_data() -> dict:
 def get_headers() -> dict:
     return get_auth_data()
 
-def print_issues(output_format, issues):
-    for issue in issues:
-        print(output_format.format(**issue))
+def _print_issue(output_format, issue):
+    print(output_format.format(**issue))
 
-def print_raw_issues(issues):
+def _print_issues(output_format, issues):
+    for issue in issues:
+        _print_issue(output_format, issue)
+
+def _print_raw_issues(issues):
     print(json.dumps(issues, sort_keys=True, indent=4))
 
 def get_file_content(filename):
     with open(filename) as f:
         return f.readlines()
+
+def _get_issue(issue, headers=None):
+    if not headers:
+        headers = get_headers()
+
+    r = requests.get(f"{JIRA_REST_URL}/issue/{issue}", headers=headers)
+    return r.json()
 
 def get_fields_mapping(project, issue_type, only_required=False):
     mapping = {}
@@ -85,8 +95,9 @@ def cmd_query(args):
     headers = get_headers()
     if args.id:
         for issue in args.id:
-            r=requests.get(f"{JIRA_REST_URL}/issue/{issue}", headers=headers)
-            output.append(r.json())
+            output.append(_get_issue(issue, headers=headers))
+            #r=requests.get(f"{JIRA_REST_URL}/issue/{issue}", headers=headers)
+            #output.append(r.json())
 
     # get issues based on url with a query
     if args.from_url:
@@ -108,9 +119,29 @@ def cmd_query(args):
         output_format = DEFAULT_OUTPUT
 
     if args.raw:
-        print_raw_issues(output)
+        _print_raw_issues(output)
     else:
-        print_issues(output_format, output)
+        _print_issues(output_format, output)
+
+def _create_issue(input_data, args):
+    headers = get_headers()
+    r=requests.post(f"{JIRA_REST_URL}/issue", json=input_data, headers=headers)
+    if not r.ok:
+        print(r.text)
+        error('Issue NOT created.')
+    new_issue = r.json()
+    # TODO: we can re-load the whole issue again to allow show other fields than key and id
+    #pprint.pprint(new_issue)
+    if args.output_format:
+        # use codecs to interpret escape characters
+        output_format = codecs.escape_decode(bytes(args.output_format, "utf-8"))[0].decode("utf-8")
+    else:
+        output_format = DEFAULT_OUTPUT
+
+    if args.raw:
+        _print_raw_issues(new_issue)
+    else:
+        _print_issue(output_format, new_issue)
 
 def cmd_create(args):
     """
@@ -132,21 +163,30 @@ def cmd_create(args):
     headers = get_headers()
     mapping = get_fields_mapping(args.project, args.issue_type, True)
     if args.json:
-        input_json = json.loads(args.json)
+        input_data = json.loads(args.json)
     elif args.json_file:
-        input_json = json.load(args.json_file)
+        input_data = json.load(args.json_file)
     else:
-        input_fields = {'project': {'key': args.project}}
+        input_fields = {'project': {'key': args.project}, 'issuetype': {'name': args.issue_type}}
         if not args.summary:
             error("Summary field is compulsory")
         input_fields['summary'] = args.summary
         if (args.description and args.description_file):
             error("Specify either --description or --description_file, but not both")
         input_fields['description'] = args.description or get_file_content(args.description_file)
-        input_json = json.dumps({'fields': input_fields}, sort_keys=True, indent=4)
-    print(input_json)
-    #r=requests.post(f"{JIRA_REST_URL}/issue", json=input_json, headers=headers)
+        input_data = {'fields': input_fields}
+    print(json.dumps(input_data, sort_keys=True, indent=4))
+    _create_issue(input_data, args)
 
+def _update_issue(issue, query):
+    headers = get_headers()
+    r = requests.put(f"{JIRA_REST_URL}/issue/{issue}", json=query, headers=headers)
+    if r.ok:
+        print(f'Issue {issue} updated.')
+    else:
+        print(f'ERROR: Issue {issue} NOT updated.')
+        print(dir(r))
+        pprint.pprint([r.reason, r.text, r.raw])
 
 def cmd_update(args):
     """
@@ -171,12 +211,74 @@ def cmd_update(args):
     if args.id:
         for issue in args.id:
             print(f'Issue {issue} being updated with: {query}')
-            r=requests.put(f"{JIRA_REST_URL}/issue/{issue}", json=query, headers=headers)
-            if r.ok:
-#            if True:
-                print(f'Issue {issue} updated.')
-            else:
-                print(f'ERROR: Issue {issue} NOT updated.')
+            _update_issue(issue, query)
+
+def cmd_clone(args):
+    """
+    Clone an issue with some logic for keeping, changing and removing some specific fields.
+    """
+    headers = get_headers()
+    issue = args.id
+    original = _get_issue(issue, headers=headers)
+    original_fields = original['fields']
+    input_fields = {}
+    input_fields['summary'] = original_fields['summary']
+    input_fields['description'] = original_fields['description']
+    input_fields['project'] = original_fields['project']
+    input_fields['issuetype'] = original_fields['issuetype']
+    clon_data = {'fields': input_fields}
+    _create_issue(clon_data, args)
+    #pprint.pprint(clon)
+
+
+def _get_transitions(issue):
+    headers = get_headers()
+    r = requests.get(f"{JIRA_REST_URL}/issue/{issue}/transitions?expand=transitions.fields", headers=headers)
+    if r.ok:
+        return r.json()['transitions']
+
+
+def _filter_transition_id(issue, status, resolution):
+    result = {}
+    transitions = _get_transitions(issue)
+    for t in transitions:
+        if t['name'] == status:
+            result['transition'] = {'id': t['id']}
+            # just for status, check the given resolution is valid
+            if status == 'Closed' and 'resolution' in t['fields']:
+                for r in t['fields']['resolution']['allowedValues']:
+                    if r['name'] == resolution:
+                        result['fields'] = {'resolution': {'name': resolution}}
+            return result
+    error(f"Cannot find a transition called '{status}' for issue '{issue}'")
+
+
+def cmd_move(args):
+    """
+    Move an issue to a different status with some comment and resolution if exists for the target status.
+    This requires transition id probably: https://issues.redhat.com/rest/api/2/issue/RHELPLAN-141790/transitions?expand=transitions.fields
+    https://community.atlassian.com/t5/Jira-questions/Close-Jira-Issue-via-REST-API/qaq-p/1845399
+    """
+    headers = get_headers()
+    if (args.comment and args.comment_file):
+        error("Specify either --comment or --comment_file, but not both")
+    issue = args.id
+    status = args.status
+    input_data = _filter_transition_id(issue, status, args.resolution)
+    r = requests.post(f"{JIRA_REST_URL}/issue/{issue}/transitions", json=input_data, headers=headers)
+    if r.ok:
+        print(f'Issue {issue} moved to {status}.')
+    else:
+        print(f'ERROR: Issue {issue} NOT transitioned.')
+        print(r.text)
+    if args.comment or args.comment_file:
+        comment_data = {'body': args.comment or get_file_content(args.comment_file)}
+        r = requests.post(f"{JIRA_REST_URL}/issue/{issue}/comment", json=comment_data, headers=headers)
+        if r.ok:
+            print(f'Comment added to the issue {issue}.')
+        else:
+            print(f'ERROR: Comment not added to the tissue {issue}.')
+            print(r.text)
 
 
 def main() -> int:
@@ -208,10 +310,22 @@ def main() -> int:
               {program_name} update -j RHELPLAN-95816 --json ' {"update": { "labels": [ {"remove": "mynewlabel"} ] } }'
 
           Creating JIRA issues:
-            Not implemented yet.
+            Pick a project (RHEL is the default), specify summary and description and create an issue.
 
             Examples:
-              TBD
+              {program_name} new --summary 'Test issue for playing around with Jira API' --description 'testing description with a nice text simulating a text for a <bug> or a <feature>.'  --project RHELPLAN
+
+          Moving to a different status and closing JIRA issues:
+            Closing a JIRA issue is just a move to a different status.
+
+            Examples:
+              {program_name} move -j RHELPLAN-141789 --status Closed --resolution 'Not a Bug' --comment 'closing'
+
+          Cloning a Jira issue:
+            This is similar to creating, the specified issue is fetched first, then we pick several fields to keep.
+
+            Examples:
+              {program_name} clone -j RHELPLAN-141789
         ''')
     # do not expand anything else than the program name, complicated format
     # would make issues when using f-strings or .format()
@@ -234,7 +348,7 @@ def main() -> int:
 
     # the idea here is to use something like print("format from user".format(**issue)) but needs to be validated by some real pythonist for security
     parser_query.add_argument('--outputformat', dest='output_format',
-                        help='Print output in the form given. Use str.format string with {key} or {field["duedate"]} syntax. Use --json to see what keys exist.')
+                        help='Print output in the form given. Use str.format string with {key} or {field["duedate"]} syntax. Use --raw to see what keys exist.')
     parser_query.set_defaults(func=cmd_query)
 
     # new command
@@ -252,6 +366,10 @@ def main() -> int:
     parser_new.add_argument('--link-subtask', help='Add a link to sub-task issue')
     parser_new.add_argument('--link-parent', help='Add a link to the parent issue')
     parser_new.add_argument('--link-epic', help='Add an epic to the new issue')
+    parser_new.add_argument('--raw', action='store_true',
+                        help='Display raw issue data (JSON)')
+    parser_new.add_argument('--outputformat', dest='output_format',
+                        help='Print output in the form given. Use str.format string with {key} or {field["duedate"]} syntax. Use --json to see what keys exist.')
 
     # update command
     parser_update = subparsers.add_parser('update', help='update a JIRA issue')
@@ -260,6 +378,38 @@ def main() -> int:
                                help='Jira issues ID')
     parser_update.add_argument('--json', metavar='json', type=str, required = True,
                                help='JSON that defines what should be changed. See "Updating an Issue via the JIRA REST APIs" section of the Jira API: https://developer.atlassian.com/server/jira/platform/updating-an-issue-via-the-jira-rest-apis-6848604/')
+    parser_update.add_argument('--comment', help='Longer comment to be added to the issue')
+    parser_update.add_argument('--comment_file', help='Longer comment to be added to issue located in a file')
+
+    # clone command
+    parser_clone = subparsers.add_parser('clone', help='clone a JIRA issue')
+    parser_clone.set_defaults(func=cmd_clone)
+    parser_clone.add_argument('-j', '--id', '--jira_id', metavar='ID', type=str, required = True,
+                               help='Jira issues ID')
+    parser_clone.add_argument('--keep', metavar='key', type=str,
+                               help='name of the key to keep in the clone (by default, keys kept are summary, description, labels)')
+    parser_clone.add_argument('--remove', metavar='key', type=str,
+                               help='name of the key to remove from the clon (by default, keys kept are summary, description, labels)')
+    parser_clone.add_argument('--set', metavar='json', type=str,
+                               help='JSON that defines what should be changed by replacing the content entirely. Example: {"summary": "My new summary"}')
+    parser_clone.add_argument('--re', metavar='json', type=str,
+                               help='JSON that defines what should be changed using regexp. The value must be a dict with keys pattern and replacement. Example: {"summary": {"pattern": "<component>", "replacement": "newcomponent"}}')
+    parser_clone.add_argument('--no-link-back', action='store_true', help='Do not link back to the original issue (if not specified, the new issue is linked back to the original one using clonned relation)')
+    parser_clone.add_argument('--raw', action='store_true',
+                        help='Display raw issue data (JSON)')
+    parser_clone.add_argument('--outputformat', dest='output_format',
+                        help='Print output in the form given. Use str.format string with {key} or {field["duedate"]} syntax. Use --json to see what keys exist.')
+
+    # move command
+    parser_move = subparsers.add_parser('move', help='change a JIRA issue status')
+    parser_move.set_defaults(func=cmd_move)
+    # so far limiting to a single issue
+    parser_move.add_argument('-j', '--id', '--jira_id', metavar='ID', type=str,
+                               help='Jira issues ID')
+    parser_move.add_argument('--comment', help='Longer comment to be added to the issue')
+    parser_move.add_argument('--comment_file', help='Longer comment to be added to issue located in a file')
+    parser_move.add_argument('--status', default='Closed', help='Target status (default: Closed)')
+    parser_move.add_argument('--resolution', default='Done', help='Resolution of the closure (default: Done)')
 
     # fields-mapping command
     parser_fields_mapping = subparsers.add_parser('fields-mapping', help='show fields mapping for a project')
